d7ac5abdfa32ec8ef216fc4fb474d04b92fe9662
Minh Duc <ducminhvu304@gmail.com>
2021-06-22 00:16:09 +0700

    akfa

--- main.py
+++ main.py
@@ -25,24 +25,31 @@ def predict():
     distance = 1000
     cur_label = 0
     arr_distance = []
-    while tmp<113:
+    arr_distance_for_average = []
+    distance_average = -1
+    while tmp < (20520/2048):
         for i in range(181):
             minh=int(train[tmp*181+i][4])
             if(minh==cur_label):
                 train[tmp*181+i].pop(4)
                 cosin_similarity = cosin.cosin_distance(test[i],train[tmp*181+i])
                 print(cosin_similarity)
-                if distance > cosin_similarity and cosin_similarity>0:
-                    distance = cosin_similarity
+                arr_distance_for_average.append(cosin_similarity)
+                # if distance > cosin_similarity and cosin_similarity>0:
+                #     distance = cosin_similarity
             else:
-                arr_distance.append(distance)
+                # arr_distance.append(distance)
+                distance_average = np.sum(arr_distance_for_average)/np.shape(arr_distance_for_average)
                 cur_label += 1
-                distance = 1000
+                # distance = 1000
+                arr_distance.append(distance_average)
+        distance_average = np.sum(arr_distance_for_average)/np.shape(arr_distance_for_average)
+        arr_distance.append(distance_average)
         tmp += 1
 
     res_label = -1
     min_distance = 100
-    print(arr_distance)
+    # print(arr_distance)
     for i in range(np.shape(arr_distance)[0]):
         if min_distance > arr_distance[i]:
             min_distance = arr_distance[i]
